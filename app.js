4ecf3ffc951a64caceb4976c3be9a7ad629a14c1
bbeizer <89622436+bbeizer@users.noreply.github.com>
2022-05-02 19:24:13 -0400

    change

--- app.js
+++ app.js
@@ -343,10 +343,11 @@ app.use(function(err, req, res, next) {
 
 
 // *********************************************************** //
-//  Starting up the server!
+//  Starting up the server! 
 // *********************************************************** //
 //Here we set the port to use between 1024 and 65535  (2^16-1)
-const port = "5000";
+const port = process.env.PORT || "5000";
+console.log('connecting on port '+port)
 app.set("port", port);
 
 // and now we startup the server listening on that port
